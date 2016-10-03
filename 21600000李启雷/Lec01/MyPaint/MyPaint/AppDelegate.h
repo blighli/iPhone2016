469a2b50eb2a7d0408b063c61bf1ac7a7f49636c
liqilei <blighli@gmail.com>
2016-10-04 01:20:34 +0800

    Init commit for iOS Project01

---
+++ 21600000李启雷/Lec01/MyPaint/MyPaint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyPaint
+//
+//  Created by liqilei on 2016/10/4.
+//  Copyright © 2016年 liqilei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 21600000李启雷/Lec01/MyPaint/MyPaint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyPaint
+//
+//  Created by liqilei on 2016/10/4.
+//  Copyright © 2016年 liqilei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

3bc79773cebce7ac82998904d38ef4c633ff5b7b
liqilei <blighli@gmail.com>
2016-10-06 00:33:56 +0800

    lql add project01

---
+++ 21600000李启雷/project01/Utils.h
@@ -0,0 +1,11 @@
+//
+//  Utils.h
+//  myCal
+//
+//  Created by liqilei on 2016/10/5.
+//  Copyright © 2016年 liqilei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+void println(NSString* strLine);

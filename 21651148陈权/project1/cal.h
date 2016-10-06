483abb63d44747886d77835db49504bfb916ef4d
Chen Quan <terasum@163.com>
2016-10-06 18:29:25 +0000

    ignore the obj files

---
+++ 21651148陈权/project1/cal.h
@@ -0,0 +1,51 @@
+# include<Foundation/Foundation.h>
+// do not output the detail information
+#define NSLog(FORMAT, ...) printf("%s\n", [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String])
+
+//printFormatter
+
+@interface MonthFormatter:NSObject
+        //当月第一天的星期数目
+        NSUInteger firstDayWeekDay;
+        //当月的最大天数
+        NSUInteger maxRangeOfMonth;
+        //目前月份
+        int monthIndator;
+       //当前年份
+         int yearIndator; 
+
+         int* monthArray;
+
+    // - (void) setFirstDayWeekDay:(NSUInteger)day;
+    - (NSMutableArray * ) getArray;
+
+    - (void) setFirstDayWeekDay:(NSUInteger)day;
+    - (NSUInteger) firstDayWeekDay;
+
+     - (void) setMaxRangeOfMonth:(NSUInteger)MaxMon;
+    - (NSUInteger) maxRangeOfMonth;
+
+    - (void) setMonthIndator:(int)monInd;
+    - (int) monthIndator;
+
+    - (void) setYearIndator:(int)yearInd;
+    - (int) yearIndator;
+
+    +(MonthFormatter * ) initByMonth:(int) m AndYear:(int) y;
+
+    + (int) judgeRangeByMonth:(int)mon AndYear:(int) year;
+  //打印方法
+    - (void) print ;
+@end
+
+@interface YearFormatter:NSObject
+   //当前年份
+    int yearIndator;
+    + (YearFormatter *) initWithYear:(int)y;
+    - (void) setYearIndator:(int)yearInd;
+    - (int) yearIndator;
+
+
+  //打印方法
+    - (void) print ;
+@end
---
+++ 21651148陈权/project1/main.m
@@ -0,0 +1,40 @@
+#include "cal.h"
+int main(int argc,const char *argv[]){
+    NSAutoreleasePool * pool =  [[NSAutoreleasePool alloc] init];
+    if (argc > 3) {
+      NSLog(@"Invalidate params");
+    }else if (argc ==2) {
+        int year = atoi(argv[1]);
+        if (year < 1970){
+          NSLog(@"年份值不合法：请使用 1970+");
+          exit(1);
+        }
+        YearFormatter  * yf = [YearFormatter initWithYear:year];
+        [yf print];
+    }else if(argc == 3){
+        int month = atoi(argv[1]);
+        if (month > 12 || month < 1){
+          NSLog(@"月份值不合法：请使用 1-12");
+          exit(1);
+        }
+        int year = atoi(argv[2]);
+        if (year < 1970){
+          NSLog(@"年份值不合法：请使用 1970+");
+          exit(1);
+        }
+        MonthFormatter *mf  = [MonthFormatter  initByMonth:month AndYear: year];
+        [mf print];
+    }else{
+      NSDateComponents *todayComps =
+          [[NSCalendar currentCalendar] components:NSDayCalendarUnit | NSMonthCalendarUnit |
+                               NSYearCalendarUnit | NSWeekdayCalendarUnit
+                      fromDate:[NSDate date]];
+      int year = (int)[todayComps year];
+      int month = (int)[todayComps month];
+      MonthFormatter *mf  = [MonthFormatter  initByMonth:month AndYear: year];
+      [mf print];
+    }
+    // [mf print];
+    [pool drain];
+    return 0;
+}

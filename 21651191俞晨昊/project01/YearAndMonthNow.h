d6205bfc743fe2c981cf33b0b6293ba33252aee9
yuchenhao <ych95101@163.com>
2016-10-07 22:35:01 +0800

    ych first

---
+++ 21651191俞晨昊/project01/YearAndMonthNow.h
@@ -0,0 +1,12 @@
+
+
+#import <Foundation/Foundation.h>
+
+@interface YearAndMonthNow : NSObject
+
+@property(nonatomic) int yearNow;
+@property(nonatomic) int monthNow;
+
+- (void) yearAndMonth;
+
+@end

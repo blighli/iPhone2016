f50d18659647aa7098016a62ba7581fba7782481
aleafall <aleafall@gmail.com>
2016-10-07 19:21:58 +0800

    calWithOc

---
+++ 21651064杨修一/project01/PrintYearAndMonth.h
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+
+@interface PrintYearAndMonth : NSObject
+
+- (void) printmonth:(int)year yue:(int)yue;
+- (void) printyear:(int)year;
+
+@end
---
+++ 21651064杨修一/project01/YearAndMonthNow.m
@@ -0,0 +1,20 @@
+
+#import "YearAndMonthNow.h"
+
+@implementation YearAndMonthNow
+
+- (void) yearAndMonth{
+    
+    NSDate *date = [NSDate date];
+    NSCalendar *cal = [NSCalendar currentCalendar];
+
+    unsigned int unitFlags = NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay;
+
+    NSDateComponents *d = [cal components:unitFlags fromDate:date];
+    
+    _yearNow = (int)[d year];
+    _monthNow = (int)[d month];
+
+}
+
+@end

a47312582f676bed474b3353f07e09756ecbc815
xiaohuihui <1161093241@qq.com>
2016-10-08 19:52:50 +0800

    ios homework,oc language

---
+++ 21651087李卓飞/project01/ErrroMsg.h
@@ -0,0 +1,19 @@
+﻿//
+//  ErrroMsg.h
+//  cal
+//
+//  Created by xiaohuihui2016 on 16/10/6.
+//  Copyright © 2016年 xiaohuihui2016. All rights reserved.
+//
+
+@interface ErrroMsg : NSObject
+
++ (const char *)getYEAR_OUT_RANGE;
++ (const char *)getMONTH_OUT_RANGE;
++ (const char *)getARG_LIMIT_EXCEED;
++ (const char *)getSUCCEED;
++ (const char *)getILLEGALOPTION;
+
+@end
+
+/* ErrroMsg_h */

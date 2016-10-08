6b22a29d714eeb75be461fe5bd6e0cfdcd003773
xuanmiao <xuanmiao@xuanmiaodeMacBook-Pro.local>
2016-10-08 20:32:55 +0800

    日历

---
+++ 21651044阮野/Project1/JudgeNumberRegex.h
@@ -0,0 +1,15 @@
+//
+//  JudgeNumberRegex.h
+//  CAL_TEST1
+//
+//  Created by xuanmiao on 16/10/7.
+//  Copyright © 2016年 xuanmiao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JudgeNumberRegex : NSObject
+
+- (BOOL) isNumber:(NSString*)str;
+
+@end

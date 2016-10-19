f67409ec3e32999204e55f29d5764f33f7354f67
NazgulLee <lijian1994@hotmail.com>
2016-10-19 20:23:04 +0800

    第二次作业和第三次作业

---
+++ 21651002李剑/project02/Calendar/Calendar/UIView+FrameProcessor.h
@@ -0,0 +1,20 @@
+//
+//  UIView+FrameProcessor.h
+//  京东金融（仿）
+//
+//  Created by 李剑 on 16/10/14.
+//  Copyright © 2016年 mutouren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (FrameProcessor)
+
+@property CGFloat centerX;
+@property CGFloat centerY;
+@property CGSize size;
+@property CGFloat x;
+@property CGFloat y;
+@property CGPoint origin;
+
+@end

a69d472711f9a5a80d65cb0fea987261c1538f6c
EzraGe <15757127192@163.com>
2016-10-19 21:30:27 +0800

    homework2

---
+++ 21651046戈宇泽/project03/Task List/Task List/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Task List
+//
+//  Created by 戈宇泽 on 16/10/16.
+//  Copyright © 2016年 戈宇泽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}

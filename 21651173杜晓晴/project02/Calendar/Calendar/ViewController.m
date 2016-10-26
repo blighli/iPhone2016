3b6cd371a408d93006618ed8a6bca17e150a4790
duxiaoqing <936066051@qq.com>
2016-10-26 20:57:58 +0800

    2,3次作业

---
+++ 21651173杜晓晴/project02/Calendar/Calendar/ViewController.m
@@ -0,0 +1,229 @@
+//
+//  ViewController.m
+//  Calendar
+//
+//  Created by duxiaoqing on 16/10/25.
+//  Copyright © 2016年 duxiaoqing. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "Formatter.h"
+
+#define LL_SCREEN_WIDTH ([UIScreen mainScreen].bounds.size.width)
+#define LL_SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
+#define Iphone6Scale(x) ((x) * LL_SCREEN_WIDTH / 375.0f)
+
+#define HeaderViewHeight 30
+#define WeekViewHeight 40
+
+@interface CalendarViewController () <UICollectionViewDelegate, UICollectionViewDataSource>
+@property (strong, nonatomic) UICollectionView *collectionView;
+@property (strong, nonatomic) NSMutableArray *dayModelArray;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+
+@property (strong, nonatomic) NSDate *tempDate;
+@end
+
+@implementation CalendarViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self.view addSubview:self.collectionView];
+    //获取当前时间
+    self.tempDate = [NSDate date];
+    //转换成yyyy－mm格式
+    self.dateLabel.text = self.tempDate.yyyyMMByLineWithDate;
+    [self getDataDayModel:self.tempDate];
+}
+
+- (void)getDataDayModel:(NSDate *)date{
+    NSUInteger days = [self numberOfDaysInMonth:date];
+    NSInteger week = [self startDayOfWeek:date];
+    //日历共需要6行
+    self.dayModelArray = [[NSMutableArray alloc] initWithCapacity:42];
+    int day = 1;
+    for (int i= 1; i<days+week; i++) {
+        if (i<week) {
+            //第一天之前输出空格
+            [self.dayModelArray addObject:@""];
+        }else{
+            //每日显示
+            MonthModel *mon = [MonthModel new];
+            mon.dayValue = day;
+            NSDate *dayDate = [self dateOfDay:day];
+            mon.dateValue = dayDate;
+            //如果该日期和当前日期相同
+            if ([dayDate.yyyyMMddByLineWithDate isEqualToString:[NSDate date].yyyyMMddByLineWithDate]) {
+                mon.isSelectedDay = YES;
+            }
+            [self.dayModelArray addObject:mon];
+            day++;
+        }
+    }
+    [self.collectionView reloadData];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return self.dayModelArray.count;
+}
+
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    CalendarCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"CalendarCell" forIndexPath:indexPath];
+    cell.dayLabel.backgroundColor = [UIColor whiteColor];
+    cell.dayLabel.textColor = [UIColor blackColor];
+    id mon = self.dayModelArray[indexPath.row];
+    if ([mon isKindOfClass:[MonthModel class]]) {
+        cell.monthModel = (MonthModel *)mon;
+    }else{
+        cell.dayLabel.text = @"";
+    }
+    
+    return cell;
+}
+
+//星期的显示
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath{
+    CalendarHeaderView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:kind withReuseIdentifier:@"CalendarHeaderView" forIndexPath:indexPath];
+    return headerView;
+}
+
+
+
+- (UICollectionView *)collectionView{
+    if (!_collectionView) {
+        NSInteger width = Iphone6Scale(54);
+        NSInteger height = Iphone6Scale(54);
+        
+        UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc]init];
+        flowLayout.itemSize = CGSizeMake(width, height);
+        flowLayout.headerReferenceSize = CGSizeMake(LL_SCREEN_WIDTH, HeaderViewHeight);
+        flowLayout.sectionInset = UIEdgeInsetsMake(0, 0, 0, 0);
+        flowLayout.minimumInteritemSpacing = 0;
+        flowLayout.minimumLineSpacing = 0;
+        
+        _collectionView = [[UICollectionView alloc]initWithFrame:CGRectMake(0, 64 + WeekViewHeight, width * 7, LL_SCREEN_HEIGHT - 64 - WeekViewHeight) collectionViewLayout:flowLayout];
+        _collectionView.delegate = self;
+        _collectionView.dataSource = self;
+        _collectionView.backgroundColor = [UIColor whiteColor];
+        
+        [_collectionView registerClass:[CalendarCell class] forCellWithReuseIdentifier:@"CalendarCell"];
+        [_collectionView registerClass:[CalendarHeaderView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"CalendarHeaderView"];
+        
+    }
+    return _collectionView;
+}
+
+
+#pragma mark -Private
+//计算当月天数
+- (NSUInteger)numberOfDaysInMonth:(NSDate *)date{
+    NSCalendar *greCalendar = [[NSCalendar alloc]initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    [greCalendar setTimeZone:[NSTimeZone timeZoneWithName:@"GMT"]];
+    return [greCalendar rangeOfUnit:NSCalendarUnitDay inUnit:NSCalendarUnitMonth forDate:date].length;
+    
+}
+
+//本月第一天
+- (NSDate *)firstDateOfMonth:(NSDate *)date{
+    NSCalendar *greCalendar = [[NSCalendar alloc]initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    [greCalendar setTimeZone:[NSTimeZone timeZoneWithName:@"GMT"]];
+    NSDateComponents *comps = [greCalendar
+                               components:NSCalendarUnitYear | NSCalendarUnitMonth |NSCalendarUnitWeekday | NSCalendarUnitDay
+                               fromDate:date];
+    comps.day = 1;
+    return [greCalendar dateFromComponents:comps];
+}
+
+//本月第一天是星期几
+- (NSUInteger)startDayOfWeek:(NSDate *)date
+{
+    NSCalendar *greCalendar = [[NSCalendar alloc]initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    [greCalendar setTimeZone:[NSTimeZone timeZoneWithName:@"GMT"]];//Asia/Shanghai
+    NSDateComponents *comps = [greCalendar
+                               components:NSCalendarUnitYear | NSCalendarUnitMonth |NSCalendarUnitWeekday | NSCalendarUnitDay
+                               fromDate:[self firstDateOfMonth:date]];
+    return comps.weekday;
+}
+
+
+
+//获取某天日期
+- (NSDate *)dateOfDay:(NSInteger)day{
+    NSCalendar *greCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    [greCalendar setTimeZone:[NSTimeZone timeZoneWithName:@"GMT"]];
+    NSDateComponents *comps = [greCalendar
+                               components:NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay
+                               fromDate:self.tempDate];
+    comps.day = day;
+    return [greCalendar dateFromComponents:comps];
+}
+
+@end
+
+//输出第一行星期
+@implementation CalendarHeaderView
+- (instancetype)initWithFrame:(CGRect)frame{
+    if (self = [super initWithFrame:frame]) {
+        
+        NSArray *weekArray = [[NSArray alloc] initWithObjects:@"日",@"一",@"二",@"三",@"四",@"五",@"六", nil];
+        
+        for (int i=0; i<weekArray.count; i++) {
+            UILabel *weekLabel = [[UILabel alloc] initWithFrame:CGRectMake(i*Iphone6Scale(54), 0, Iphone6Scale(54), HeaderViewHeight)];
+            weekLabel.textAlignment = NSTextAlignmentCenter;
+            weekLabel.textColor = [UIColor grayColor];
+            weekLabel.font = [UIFont systemFontOfSize:13.f];
+            weekLabel.text = weekArray[i];
+            [self addSubview:weekLabel];
+        }
+        
+    }
+    return self;
+}
+@end
+
+
+@implementation CalendarCell
+- (instancetype)initWithFrame:(CGRect)frame{
+    if (self = [super initWithFrame:frame]) {
+        CGFloat width = self.contentView.frame.size.width*0.6;
+        CGFloat height = self.contentView.frame.size.height*0.6;
+        //每天
+        UILabel *dayLabel = [[UILabel alloc] initWithFrame:CGRectMake( self.contentView.frame.size.width*0.5-width*0.5,  self.contentView.frame.size.height*0.5-height*0.5, width, height )];
+        dayLabel.textAlignment = NSTextAlignmentCenter;
+        dayLabel.layer.masksToBounds = YES;
+        dayLabel.layer.cornerRadius = height * 0.5;
+        
+        [self.contentView addSubview:dayLabel];
+        self.dayLabel = dayLabel;
+        
+    }
+    return self;
+}
+
+- (void)setMonthModel:(MonthModel *)monthModel{
+    _monthModel = monthModel;
+    //所选日期显示
+    self.dayLabel.text = [NSString stringWithFormat:@"%02ld",monthModel.dayValue];
+}
+@end
+
+
+@implementation MonthModel
+
+@end
+
---
+++ 21651173杜晓晴/project02/Calendar/Calendar/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Calendar
+//
+//  Created by duxiaoqing on 16/10/25.
+//  Copyright © 2016年 duxiaoqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
+

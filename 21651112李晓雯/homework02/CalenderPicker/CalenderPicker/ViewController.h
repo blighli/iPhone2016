52e8a6f6614bd7128101f5869ed9e4a7d83689e5
Tsunami-lj <727155384@qq.com>
2016-10-27 13:16:31 +0800

    homework02

---
+++ 21651112李晓雯/homework02/CalenderPicker/CalenderPicker/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  CalendarPicker
+//
+//  Created by li xiaowen on 16/10/19.
+//  Copyright © 2016年 li xiaowen. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 21651112李晓雯/homework02/CalenderPicker/CalenderPicker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CalenderPicker
+//
+//  Created by MrYu on 2016/10/27.
+//  Copyright © 2016年 li xiaowen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
